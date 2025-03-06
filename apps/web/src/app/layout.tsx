8828a0eb1752892239e5255000a296bfe4c0fc72
Christian Batista <christian.batista@norus.com.br>
2025-03-06 14:03:13 -0300

    refactor: conditionally load script based on vercel env

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,6 +4,8 @@ import type { Viewport } from "next/types"
 
 import { ThemeProvider } from "next-themes"
 
+import { env } from "@grek/env/web"
+
 import { Toaster } from "@/components/ui/sonner"
 import { cn } from "@/lib/utils"
 import { QueryProvider } from "@/providers/query-provider"
@@ -21,10 +23,12 @@ const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script
-          crossOrigin="anonymous"
-          src="//unpkg.com/react-scan/dist/auto.global.js"
-        />
+        {env.VERCEL_ENV !== "production" && (
+          <script
+            crossOrigin="anonymous"
+            src="//unpkg.com/react-scan/dist/auto.global.js"
+          />
+        )}
       </head>
       <body vaul-drawer-wrapper="" className={cn(fontSans, fontMono)}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
